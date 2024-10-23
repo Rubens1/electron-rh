a271d3e5793e09269606db1a87759260e01b9b68
Rubens Nogueira <rubens.jesus1997@gmail.com>
2024-10-23 10:38:49 -0300

    Projeto de ponto

---
+++ src/renderer/src/components/Loading.jsx
@@ -0,0 +1,9 @@
+export default function Loading() {
+    return (
+        <>
+            <div id="loader-wrapper">
+                <div id="loader"></div>
+            </div>
+        </>
+    )
+}
